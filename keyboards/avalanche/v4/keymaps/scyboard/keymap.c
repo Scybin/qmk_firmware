39a9352d64f60dcfc02baa16f36e862f2024f86c
Scybin <xheavyice@gmail.com>
2025-04-26 17:01:04 -0600

    Update keymap

--- keyboards/avalanche/v4/keymaps/scyboard/keymap.c
+++ keyboards/avalanche/v4/keymaps/scyboard/keymap.c
@@ -31,29 +31,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_ESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
                      KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         TD_LALT_DF2, KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RBRC,
-                     TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DF(RGB),  KC_UP,   KC_DEL,  KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                                          KC_LEFT, KC_RGHT, KC_LWIN, KC_SPC,   KC_DOWN, KC_INS,  KC_ENT,  KC_BSPC, KC_HOME, KC_END
-    ),                                                                                         
-    [LIGHT] = LAYOUT(                                                                            
-                     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_VALD, UG_VALU,                                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_SATD, UG_SATU,                                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX,     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_HUED, UG_HUEU,                                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_PREV, UG_NEXT, DF(BASE), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                                          XXXXXXX, UG_SPDD, UG_SPDU, RGB_TOG,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-    ),                                                                                         
-    [DEV] = LAYOUT(                                                                            
-                     _______,           _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
-                     _______,           _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
-        _______,     _______,           _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______, _______,
-                     _______,           _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                                          _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______
-    ),                                                                                         
-    [OSRS] = LAYOUT(                                                                           
-                     KC_ESC,            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-                     KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-        TD_LALT_DF0, KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSPC,
-                     TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RGB_TOG,  KC_UP,   KC_DEL,  KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                                          KC_BTN1, KC_LCTL, KC_LSFT, KC_SPC,   XXXXXXX, XXXXXXX, KC_ENT,  KC_BSPC, XXXXXXX, XXXXXXX
+                     TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    DF(LIGHT), KC_UP,   KC_DEL,  KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                                                          KC_LEFT, KC_RGHT, KC_LWIN, KC_SPC,    KC_DOWN, KC_INS,  KC_ENT,  KC_BSPC, KC_HOME, KC_END
+    ),                                                                                          
+    [LIGHT] = LAYOUT(                                                                             
+                     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_VALD, UG_VALU,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_SATD, UG_SATU,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_HUED, UG_HUEU,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                     XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, UG_PREV, UG_NEXT, DF(BASE),  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                                          XXXXXXX, UG_SPDD, UG_SPDU, RGB_TOG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),                                                                                          
+    [DEV] = LAYOUT(                                                                             
+                     _______,           _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
+                     _______,           _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
+        _______,     _______,           _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______, _______,
+                     _______,           _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                                                          _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______
+    ),                                                                                          
+    [OSRS] = LAYOUT(                                                                            
+                     KC_ESC,            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+                     KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+        TD_LALT_DF0, KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSPC,
+                     TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RGB_TOG,   KC_UP,   KC_DEL,  KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                                                          KC_BTN1, KC_LCTL, KC_LSFT, KC_SPC,    XXXXXXX, XXXXXXX, KC_ENT,  KC_BSPC, XXXXXXX, XXXXXXX
     ),
 };
 
