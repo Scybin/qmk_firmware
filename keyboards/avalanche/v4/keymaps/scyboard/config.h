f542d4ab060791a8eb182644d3310cac1cc8b5a7
Scybin <xheavyice@gmail.com>
2025-04-27 14:35:58 -0600

    Update default RGBLight Mode to Breathing

--- keyboards/avalanche/v4/keymaps/scyboard/config.h
+++ keyboards/avalanche/v4/keymaps/scyboard/config.h
@@ -2,14 +2,7 @@
 
 // Override the default RGBLight mode
 #undef RGBLIGHT_DEFAULT_MODE
-#define RGBLIGHT_ANIMATIONS            // Enable RGB animations (required for static mode)
-#define RGBLIGHT_DEFAULT_MODE 1T
-#define RGBLIGHT_DEFAULT_HUE 170       // Default hue (blue)
-#define RGBLIGHT_DEFAULT_SAT 255       // Default saturation (maximum)
-#define RGBLIGHT_DEFAULT_VAL 255       // Default brightness (maximum)
-#define RGBLIGHT_SLEEP                 // Turn off RGB lighting when the host goes to sleep
-#define RGBLIGHT_SAT_STEP 8            // Controls the saturation change step
-#define RGBLIGHT_VAL_STEP 8            // Controls the value change step
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING
 
 // Tapping configuration
 #define TAPPING_TERM 200  // 200ms tapping term
