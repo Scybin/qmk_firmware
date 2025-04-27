20ead058411f3d7414a4453f29b4caa3b525c6e1
Scybin <xheavyice@gmail.com>
2025-04-26 18:52:23 -0600

    Update config.h

--- keyboards/avalanche/v4/keymaps/scyboard/config.h
+++ keyboards/avalanche/v4/keymaps/scyboard/config.h
@@ -1,5 +1,4 @@
 #define TAPPING_TERM 200  // 200ms tapping term
 
-#define RGBLED_NUM 64          // Number of RGB LEDs
 #define RGBLIGHT_SAT_STEP 8    // Controls the saturation change step
 #define RGBLIGHT_VAL_STEP 8    // Controls the value change step
