fdfbb4a46b73708c7ed77d2e67f9204a2174e204
Scybin <xheavyice@gmail.com>
2025-04-27 13:25:17 -0600

    Update OLED

--- keyboards/avalanche/v4/keymaps/scyboard/keymap.c
+++ keyboards/avalanche/v4/keymaps/scyboard/keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,      KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC,
         TD(LOSRS), KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,      KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, KC_RBRC,
                    TD(SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      TD(LLIGHT), XXXXXXX, XXXXXXX, KC_RALT, KC_N,      KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_BSLS,
-                                                     QK_BOOT, EE_CLR,  MO(LOWER), KC_SPC,     KC_LWIN, KC_ENT,  KC_BSPC, MO(UPPER), XXXXXXX, DF(OSRS)
+                                                     EE_CLR,  XXXXXXX, MO(LOWER), KC_SPC,     KC_LWIN, KC_ENT,  KC_BSPC, MO(UPPER), XXXXXXX, DF(OSRS)
     ),                                                                                                                                             
     [LIGHT] = LAYOUT(                                                                                                                              
                    XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, UG_VALD, UG_VALU,                                          XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
--- keyboards/avalanche/v4/keymaps/scyboard/oled.c
+++ keyboards/avalanche/v4/keymaps/scyboard/oled.c
@@ -1,6 +1,12 @@
 #include QMK_KEYBOARD_H
 
 bool oled_task_user(void) {
+    // Static variables to store the last pressed key's information
+    static uint8_t last_row = 0;
+    static uint8_t last_col = 0;
+    static uint16_t last_keycode = 0;
+    static bool key_pressed = false;
+
     // Custom OLED logic
     if (is_keyboard_master()) {
         oled_clear();
@@ -37,32 +43,30 @@ bool oled_task_user(void) {
         oled_set_cursor(0, 4);
 
         // Iterate through the matrix to find a pressed key
-        bool key_found = false;
-        uint8_t pressed_row = 0;
-        uint8_t pressed_col = 0;
-        uint16_t keycode = 0;
-
+        bool current_key_found = false;
         for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
             for (uint8_t col = 0; col < MATRIX_COLS; col++) {
                 if (matrix_is_on(row, col)) {
-                    key_found = true;
-                    pressed_row = row;
-                    pressed_col = col;
+                    current_key_found = true;
 
-                    // Get the keycode for the current layer
-                    keycode = keymap_key_to_keycode(current_layer, (keypos_t){.row = row, .col = col});
+                    // Update the last pressed key's information
+                    last_row = row;
+                    last_col = col;
+                    last_keycode = keymap_key_to_keycode(current_layer, (keypos_t){.row = row, .col = col});
+                    key_pressed = true;
+
                     break; // Exit inner loop
                 }
             }
-            if (key_found) {
+            if (current_key_found) {
                 break; // Exit outer loop
             }
         }
 
-        // Display row and column or "None" if no key is pressed
-        if (key_found) {
+        // Display row and column
+        if (key_pressed) {
             char row_col_str[16];
-            snprintf(row_col_str, sizeof(row_col_str), "Row: %d Col: %d", pressed_row, pressed_col);
+            snprintf(row_col_str, sizeof(row_col_str), "Row: %d Col: %d", last_row, last_col);
             oled_write(row_col_str, false);
         } else {
             oled_write_P(PSTR("Row: None Col: None"), false);
@@ -71,10 +75,10 @@ bool oled_task_user(void) {
         // Move cursor two rows below the row and column display
         oled_set_cursor(0, 6);
 
-        // Display the keycode or "None" if no key is pressed
-        if (key_found) {
+        // Display the keycode
+        if (key_pressed) {
             char keycode_str[16];
-            snprintf(keycode_str, sizeof(keycode_str), "KC: 0x%X", keycode);
+            snprintf(keycode_str, sizeof(keycode_str), "KC: 0x%X", last_keycode);
             oled_write(keycode_str, false);
         } else {
             oled_write_P(PSTR("KC: None"), false);
