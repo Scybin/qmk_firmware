9a04afed54833efb843a43c11807533a8cda4cc2
Scybin <xheavyice@gmail.com>
2025-04-27 09:28:51 -0600

    Update

--- keyboards/avalanche/v4/keymaps/scyboard/config.h
+++ keyboards/avalanche/v4/keymaps/scyboard/config.h
@@ -4,23 +4,19 @@
 #define TAPPING_TERM 200  // 200ms tapping term
 
 // RGB Matrix configuration
-#define RGB_DI_PIN D3
-
-#ifdef RGB_MATRIX_ENABLE
-
-#define RGBLED_NUM 64
+#define RGB_MATRIX_ENABLE
+#define RGB_DI_PIN D3  // This matches the "pin" defined in the `ws2812` section of your keyboard.json
+#define RGBLED_NUM 64   // This matches the "led_count" defined in the `rgblight` section of your keyboard.json
 #define DRIVER_LED_TOTAL RGBLED_NUM
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // limits maximum brightness of LEDs to 120 out of 255. Higher may cause the controller to crash.
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
 #define RGB_MATRIX_HUE_STEP 8
 #define RGB_MATRIX_SAT_STEP 8
 #define RGB_MATRIX_VAL_STEP 8
 #define RGB_MATRIX_SPD_STEP 10
-#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_KEYPRESSES  // Enable reactive effects
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define RGB_MATRIX_SPLIT {32,32}
+#define RGB_MATRIX_SPLIT {32, 32}  // Split configuration for 32 LEDs per side
 #define SPLIT_TRANSPORT_MIRROR
-
-#endif
 
 // OLED configuration
 #ifdef OLED_ENABLE
