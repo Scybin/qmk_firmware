2516800d53a328df0fab4163aae940a67ceb51c4
Scybin <xheavyice@gmail.com>
2025-04-27 09:43:50 -0600

    Update

--- keyboards/avalanche/v4/keymaps/scyboard/rules.mk
+++ keyboards/avalanche/v4/keymaps/scyboard/rules.mk
@@ -3,6 +3,7 @@ MOUSEKEY_ENABLE = yes                # Mouse key support
 TAP_DANCE_ENABLE = yes               # Tap dance functionality
 RGBLIGHT_ENABLE = no                 # RGB lighting
 RGB_MATRIX_ENABLE = yes              # RGB matrix
+RGB_MATRIX_DRIVER = WS2812_DRIVER
 OLED_ENABLE = no                     # OLED support
 
 # Optimization settings
