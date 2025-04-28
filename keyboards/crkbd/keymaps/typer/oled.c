5c2346ff06c8e94af52c33b4da7472c7c4c5c829
Scybin <xheavyice@gmail.com>
2025-04-28 11:42:20 -0600

    Define custom oled task

--- keyboards/crkbd/keymaps/typer/oled.c
+++ keyboards/crkbd/keymaps/typer/oled.c
@@ -1,11 +1,11 @@
 #include QMK_KEYBOARD_H
 
-#ifdef OLED_TASK_KB_OVERRIDE
+// Override the parent implementation of oled_task_kb
 bool oled_task_kb(void) {
-    return oled_task_user();
+    return oled_task_user(); // Use your custom user-defined OLED task
 }
-#endif
 
+// Custom user-defined OLED task
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         oled_write_ln("Master Side", false);
@@ -13,4 +13,13 @@ bool oled_task_user(void) {
         oled_write_ln("Slave Side", false);
     }
     return true;
+}
+
+// Optional: Override other parent functions if needed
+void oled_render_layer_state(void) {
+    oled_write_ln("Custom Layer State", false);
+}
+
+void oled_render_logo(void) {
+    oled_write_ln("Custom Logo", false);
 }
