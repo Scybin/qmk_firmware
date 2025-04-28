9ab9935e6d72397a45efddb8cbda891537551c3f
Scybin <xheavyice@gmail.com>
2025-04-28 07:59:05 -0600

    Optimize

--- keyboards/crkbd/keymaps/typer/keymap.c
+++ keyboards/crkbd/keymaps/typer/keymap.c
@@ -19,11 +19,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+       KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO,  RGB_TOG, RGB_HUD, RGB_HUI, RGB_RMOD, RGB_MOD,                     KC_TRNS,  KC_UP,  KC_TRNS, KC_TRNS, KC_DEL,  KC_HOME,
+      KC_TRNS,  RGB_TOG, RGB_HUD, RGB_HUI, RGB_RMOD, RGB_MOD,                     KC_TRNS,  KC_UP,  KC_TRNS, KC_TRNS, KC_DEL,  KC_HOME,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO,  KC_CAPS, RGB_SAD, RGB_SAI, RGB_SPD, RGB_SPI,                      KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_INS, KC_END,
+      KC_TRNS,  KC_CAPS, RGB_SAD, RGB_SAI, RGB_SPD, RGB_SPI,                      KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_INS, KC_END,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          KC_LALT, KC_TRNS,  KC_SPC,    BSPC_ENT, MO(3),  KC_TRNS
                                       //`--------------------------'  `--------------------------'
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB, KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_TRNS, KC_NO,  KC_TRNS,  KC_NO,   KC_NO,                        KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
+      KC_LCTL, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,                        KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,                        KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_LSFT, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,                        KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, KC_TRNS,  KC_SPC,   BSPC_ENT, KC_RALT, KC_TRNS
                                       //`--------------------------'  `--------------------------'
