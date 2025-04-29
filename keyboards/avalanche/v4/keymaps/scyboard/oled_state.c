ba43a8c9d3a75860563cb566fb161f0f5feb8cb4
Scybin <xheavyice@gmail.com>
2025-04-29 08:37:32 -0600

    Implement OLED timeout

--- keyboards/avalanche/v4/keymaps/scyboard/oled_state.c
+++ keyboards/avalanche/v4/keymaps/scyboard/oled_state.c
@@ -1,15 +1,20 @@
 #include "oled_state.h"
 
+// State variables
 static uint32_t total_characters = 0;
 static matrix_row_t previous_matrix[MATRIX_ROWS] = {0};
 static uint8_t last_row = 0, last_col = 0;
 static uint16_t last_keycode = 0;
+
+// Timeout variables
+static uint32_t oled_last_activity = 0;
 
 void oled_state_init(void) {
     total_characters = 0;
     memset(previous_matrix, 0, sizeof(previous_matrix));
     last_row = last_col = 0;
     last_keycode = 0;
+    oled_last_activity = timer_read32(); // Initialize the timer
 }
 
 void update_key_state(uint8_t row, uint8_t col, uint8_t layer, bool is_pressed) {
@@ -18,6 +23,7 @@ void update_key_state(uint8_t row, uint8_t col, uint8_t layer, bool is_pressed) 
         last_col = col;
         last_keycode = keymap_key_to_keycode(layer, (keypos_t){.row = row, .col = col});
         total_characters++;
+        reset_oled_timer(); // Reset the timer on key press
     }
 }
 
@@ -43,4 +49,13 @@ matrix_row_t get_previous_matrix_row(uint8_t row) {
 
 void set_previous_matrix_row(uint8_t row, matrix_row_t value) {
     previous_matrix[row] = value;
+}
+
+// Timeout-related functions
+void reset_oled_timer(void) {
+    oled_last_activity = timer_read32();
+}
+
+bool is_oled_timed_out(void) {
+    return (timer_elapsed32(oled_last_activity) > OLED_TIMEOUT); // Use OLED_TIMEOUT from config.h
 }
