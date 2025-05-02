f57415e1bcc2c8a517a1db0203980ff07fa1d1c1
Scybin <xheavyice@gmail.com>
2025-05-02 12:13:26 -0600

    Compile

--- keyboards/crkbd/keymaps/typer/config.h
+++ keyboards/crkbd/keymaps/typer/config.h
@@ -1,14 +1,14 @@
-#ifdef RGB_MATRIX_ENABLE
-#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+//# ifdef RGB_MATRIX_ENABLE
+//#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 //#   define RGB_MATRIX_SLEEP // turn off effects when suspended
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+//#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 // #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 // #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#    define RGB_MATRIX_HUE_STEP 8
-#    define RGB_MATRIX_SAT_STEP 8
-#    define RGB_MATRIX_VAL_STEP 8
-#    define RGB_MATRIX_SPD_STEP 10
+//#define RGB_MATRIX_HUE_STEP 8
+//#define RGB_MATRIX_SAT_STEP 8
+//#define RGB_MATRIX_VAL_STEP 8
+//#define RGB_MATRIX_SPD_STEP 10
 
 //#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
 //#    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
@@ -58,12 +58,18 @@
 #endif
 
 // OLED configuration
-#ifdef OLED_ENABLE
-#    define SPLIT_OLED_ENABLE
-#    define OLED_TIMEOUT 30000  // Timeout after 30 seconds of inactivity
+# ifdef OLED_ENABLE
+#define SPLIT_OLED_ENABLE
+#define OLED_TIMEOUT 30000  // Timeout after 30 seconds of inactivity
 #endif
 
 #define OLED_TASK_KB_OVERRIDE
 
 #define RGB_MATRIX_CUSTOM_EFFECTS
 #define ENABLE_RGB_MATRIX_CUSTOM_TYPING_HEATMAP
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_HUE_STEP 8
+#define RGB_MATRIX_SAT_STEP 8
+#define RGB_MATRIX_VAL_STEP 8
+#define RGB_MATRIX_SPD_STEP 10
