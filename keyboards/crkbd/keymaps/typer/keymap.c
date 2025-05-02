0875c2be3c3011e67bf010277b6407551d1148fb
Scybin <xheavyice@gmail.com>
2025-05-02 10:46:24 -0600

    compile

--- keyboards/crkbd/keymaps/typer/keymap.c
+++ keyboards/crkbd/keymaps/typer/keymap.c
@@ -88,7 +88,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_mode(RGB_MATRIX_TYPING_HEATMAP_WITH_UNDERGLOW);
+    rgb_matrix_mode(TYPING_HEATMAP_WITH_UNDERGLOW);
     rgb_matrix_sethsv(170, 255, 255);
 
     autocorrect_enable();
