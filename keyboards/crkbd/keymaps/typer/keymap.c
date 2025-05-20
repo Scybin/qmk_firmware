eb7b69c62b15d255cd6bc5da899dd9d875761030
Scybin <xheavyice@gmail.com>
2025-05-20 08:54:01 -0600

    Compile

--- keyboards/crkbd/keymaps/typer/keymap.c
+++ keyboards/crkbd/keymaps/typer/keymap.c
@@ -95,6 +95,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_mode(RGB_MATRIX_CUSTOM_BLUE_HEATMAP);
+    rgb_matrix_mode(RGB_MATRIX_BLUE_HEATMAP);
     autocorrect_enable();
 }
