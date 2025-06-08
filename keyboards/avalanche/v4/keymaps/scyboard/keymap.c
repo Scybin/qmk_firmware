93da499178fd4768618cc6fbbd1a79e93a94c067
Scybin <xheavyice@gmail.com>
2025-06-07 19:26:29 -0600

    Compile

--- keyboards/avalanche/v4/keymaps/scyboard/keymap.c
+++ keyboards/avalanche/v4/keymaps/scyboard/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,      KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC,
         TD(LOSRS), KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,      KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, KC_RBRC,
                    TD(SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      TD(LLIGHT), XXXXXXX, XXXXXXX, KC_RALT, KC_N,      KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_BSLS,
-                                                     EE_CLR,  XXXXXXX, MO(LOWER), KC_SPC,     KC_LWIN, KC_ENT,  KC_BSPC, MO(UPPER), XXXXXXX, DF(OSRS)
+                                                     EE_CLR,  XXXXXXX, MO(LOWER), KC_SPC,     KC_LWIN, KC_ENT,  KC_BSPC, MO(UPPER), DF(DEV), DF(OSRS)
     ),                                                                                                                                             
     [LIGHT] = LAYOUT(                                                                                                                              
                    XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, UG_VALD, UG_VALU,                                          XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
@@ -101,9 +101,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEV] = LAYOUT(                                                                                                                                    
                    _______,        _______, _______, _______, _______, _______,                                          _______,   _______, _______,  _______, _______, _______,
                    _______,        _______, _______, _______, _______, _______,                                          _______,   _______, _______,  _______, _______, _______,
-        _______,   _______,        _______, _______, _______, _______, _______,                                          _______,   _______, _______,  _______, _______, _______, _______,
-                   _______,        _______, _______, _______, _______, _______,   _______,    _______, _______, _______, _______,   _______, _______,  _______, _______, _______,
-                                                     _______, _______, _______,   _______,    _______, _______, _______, _______,   _______, _______   
+        _______,   _______,        _______, _______, _______, KC_W,    _______,                                          _______,   KC_UP,   _______,  _______, _______, _______, _______,
+                   _______,        _______, _______, KC_A,    KC_S,    KC_D,      _______,    _______, _______, _______, KC_LEFT,   KC_DOWN, KC_RIGHT, _______, _______, _______,
+                                                     _______, _______, _______,   _______,    _______, _______, _______, _______,   DF(BASE),_______   
     ),                                                                                                                                                 
     [OSRS] = LAYOUT(                                                                                                                                   
                    _______,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                          XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
